1fa370e160bf98d6b1ba6cc91554cc81a03a321c
nodebreaker0-0 <zeroday26@gmail.com>
2020-08-04 01:52:41 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module githup.com/nodebreaker0-0/gnark-rollup-exp
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nodebreaker0-0/gnark-rollup-exp/cmd"
+
+func main() {
+	cmd.Execute()
+}
